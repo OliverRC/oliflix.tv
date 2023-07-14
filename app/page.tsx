097f2d90f36b1b409de279b8d24b2d3c9dd31d68
Oliver Rivett-Carnac <oliverrc@gmail.com>
2023-07-14 09:51:49 +0200

    Make it all pretty like

--- app/page.tsx
+++ app/page.tsx
@@ -25,13 +25,13 @@ export default function Home() {
     name: 'Radarr',
     url: 'https://radarr.oliflix.tv',
     logo: '/radarr-logo.png',
-    description: 'Handles movie stuff!'
+    description: 'Manages movie stuff!'
   },
   {
     name: 'Sonarr',
     url: 'https://sonarr.oliflix.tv',
     logo: '/sonarr-logo.svg',
-    description: 'Handles series/tv/anime stuff!'
+    description: 'Manages series/tv/anime stuff!'
   },
   {
     name: 'Tautalli',
@@ -43,11 +43,18 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
 
-      <div className="text-5xl font-extrabold ...">
-        <span className="bg-clip-text text-transparent bg-gradient-to-r from-indigo-500 to-indigo-600">
-          The home of sweet sweet pixel goodness
-        </span>
+      <Image src="/oliflix-icon.svg" alt="Oliflix" width={60} height={60} className='absolute top-8 left-8' />
+
+      <div className='flex flex-col items-center p-10'>
+
+        <Image src="/oliflix-text.svg" width={400} height={200} alt='Oliflix' className='mx-auto pb-8 ' />
+        <div className="text-5xl font-extrabold">
+          <span className="bg-clip-text text-transparent bg-gradient-to-br from-rose-500 via-purple-500 to-indigo-600">
+            Lights, camera, bingewatch!
+          </span>
+        </div>
       </div>
+
 
       <div>
         <ul className="space-y-4">
@@ -61,7 +68,7 @@ export default function Home() {
         </ul>
       </div>
 
-      <span>bye bye meow</span>
+      <span className='text-xs bg-clip-text text-transparent bg-gradient-to-br from-rose-500 via-purple-500 to-indigo-600'>hello how are you okay bye...</span>
     </main>
   )
 }
@@ -70,16 +77,18 @@ export default function Home() {
 
 function LinkElement({ link }: { link: Link }) {
   return (
-    <div className="grid grid-cols-2 px-6 py-4 bg-indigo-700/20 rounded-lg space-x-6 hover:bg-indigo-700/30 transition duration-500 hover:scale-105 transform items-center">
-      <div className='justify-self-center'>
-        <Image src={link.logo} alt={link.name} width={200} height={100} className='w-auto h-12' />
+    <a href={link.url} target='_blank'>
+      <div className="grid grid-cols-2 px-6 py-4 bg-indigo-700/20 rounded-lg space-x-6 hover:bg-indigo-700/30 transition duration-500 hover:scale-105 transform items-center">
+        <div className='justify-self-start'>
+          <Image src={link.logo} alt={link.name} width={200} height={100} className='w-auto h-12' />
+        </div>
+        <div className='justify-self-end flex'>
+          <span className="text-sm text-indigo-200 pl-12 pr-4">{link.description}</span>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-6 h-6 stroke-indigo-300/60">
+            <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
+          </svg>
+        </div>
       </div>
-      <div className='justify-self-end flex'>
-        <span className="text-sm text-indigo-200 pl-12 pr-4">{link.description}</span>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" className="w-6 h-6 stroke-indigo-300/60">
-          <path stroke-linecap="round" stroke-linejoin="round" d="M13.5 6H5.25A2.25 2.25 0 003 8.25v10.5A2.25 2.25 0 005.25 21h10.5A2.25 2.25 0 0018 18.75V10.5m-10.5 6L21 3m0 0h-5.25M21 3v5.25" />
-        </svg>
-      </div>
-    </div>
+    </a>
   )
 }
